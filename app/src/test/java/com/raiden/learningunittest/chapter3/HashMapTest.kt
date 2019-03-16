686253866cb458e8d68f49cf42b3433746ad2a98
Paul Karpukhin <ruberoid55@gmail.com>
2019-03-16 11:51:29 +0600

    Verify clear method of the HashMap class

--- app/src/test/java/com/raiden/learningunittest/chapter3/HashMapTest.kt
+++ app/src/test/java/com/raiden/learningunittest/chapter3/HashMapTest.kt
@@ -2,6 +2,7 @@ package com.raiden.learningunittest.chapter3
 
 import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 
@@ -19,7 +20,7 @@ class HashMapTest {
     }
 
     @After
-    fun clearHashMap(){
+    fun clearHashMap() {
         hashMap.clear()
     }
 
@@ -29,9 +30,15 @@ class HashMapTest {
     }
 
     @Test
-    fun  `hashMap must replace value`(){
+    fun `hashMap must replace value`() {
         val newValue = "NEW VALUE"
         hashMap[KEY] = newValue
         assertEquals(newValue, hashMap[KEY])
+    }
+
+    @Test
+    fun `hashMap must be cleaned`() {
+        hashMap.clear()
+        assertTrue(hashMap.isEmpty())
     }
 }
