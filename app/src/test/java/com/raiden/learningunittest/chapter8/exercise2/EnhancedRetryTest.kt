f5feaeb36d75744989b67125c596f772ce234336
Paul Karpukhin <ruberoid55@gmail.com>
2019-05-17 15:52:10 +0600

    renamed method name

--- app/src/test/java/com/raiden/learningunittest/chapter8/exercise2/EnhancedRetryTest.kt
+++ app/src/test/java/com/raiden/learningunittest/chapter8/exercise2/EnhancedRetryTest.kt
@@ -7,30 +7,26 @@ import org.junit.Test
 public class EnhancedRetryTest {
     @get:Rule
     public var retryTestRule = RetryTestEnhancedRule()
-
+    var firstTestCounter = 0
+    var secondTestCounter = 0
+    var thirdTestCounter = 0
     @Test
     @Retry(times = 3)
-    fun shouldFailOnSecondAttempt() {
+    fun `should fail on secondAttempt`() {
         firstTestCounter++
         Assert.fail("failing $firstTestCounter")
     }
 
     @Test
     @Retry(times = 4)
-    fun shouldFailOnThirdAttempt() {
+    fun `should fail on third attempt`() {
         secondTestCounter++
         Assert.fail("failing $secondTestCounter")
     }
 
     @Test
-    fun shouldNotBeRerun() {
+    fun `should not be rerun`() {
         thirdTestCounter++
         Assert.fail("failing $thirdTestCounter")
-    }
-
-    companion object {
-        var firstTestCounter = 0
-        var secondTestCounter = 0
-        var thirdTestCounter = 0
     }
 }
