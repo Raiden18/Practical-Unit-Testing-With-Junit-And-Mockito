5c89906ddc025dc82564ab0a7c4da240f887cea2
Paul Karpukhin <ruberoid55@gmail.com>
2019-05-17 15:18:17 +0600

    Added test for the 7th chapter

--- app/src/test/java/com/raiden/learningunittest/chapter7/MailClientTest.kt
+++ app/src/test/java/com/raiden/learningunittest/chapter7/MailClientTest.kt
@@ -20,9 +20,9 @@ class MailClientTest() {
         val email = Email("123", "123", "123")
         mockStatic(EmailServer::class.java)
 
-        val address = "Wide Street"
-        val title = "Great time"
-        val body = "Hi guys!, It was really great"
+        val address = "ruberoid55@gmail.com"
+        val title = "Пиьсмо бабушке"
+        val body = "Где пенсия?"
 
         PowerMockito.whenNew(Email::class.java).withArguments(address, title, body).thenReturn(email)
         legacyMailClient.sendEmail(address, title, body)
